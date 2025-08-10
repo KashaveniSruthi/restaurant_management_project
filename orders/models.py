3efddf18bbc1940b79523c7007fcc17a9db3f8c7
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-08-10 10:39:40 +0530

    25173

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class RestaurantInfo(models.Model):
-    name=models.CharField(max_length=255)
-    def__str__(self):
-        return self.name
+
