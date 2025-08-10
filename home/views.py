28c3c9b1978cdfaaa4520dcf46edc82a455f9abd
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-08-10 11:37:28 +0530

    25174

--- home/views.py
+++ home/views.py
@@ -4,10 +4,11 @@ from django.shortcuts import render
 from .models import RestaurantInfo
 def homepage(request):
     restaurant=RestaurantInfo.objects.first()
-    return render(request,'home.html',{'restaurant_name':restaurant_name if restaurant else 'Our restaurant'})
+    return render(request,'home.html',{'restaurant_name':restaurant.name if restaurant else 'Our restaurant'})
 def about(request):
     restaurant=RestaurantInfo.objects.first()
     return  render(requesr,'about.html',{
-        'restaurant_name':restaurant_name if restaurant else 'Our restaurant'
-    
+        'restaurant_name':restaurant.name if restaurant else 'Our restaurant',
+        'description':restaurant.description if restaurant else '',
+        'image': restaurant.image.url if restaurant and restaurant.image else ''
     })
