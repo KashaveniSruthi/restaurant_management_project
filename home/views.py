f1e53af4076d902b36f7d84445fe82b8556e1035
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-08-10 11:33:59 +0530

    25174

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from .models import RestaurantInfo
 def homepage(request):
     restaurant=RestaurantInfo.objects.first()
     return render(request,'home.html',{'restaurant_name':restaurant_name if restaurant else 'Our restaurant'})
+def about(request):
+    restaurant=RestaurantInfo.objects.first()
+    return  render(requesr,'about.html',{
+        'restaurant_name':restaurant_name if restaurant else 'Our restaurant'
+    
+    })
