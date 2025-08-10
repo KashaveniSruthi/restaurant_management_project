f67169053dcadf971acd4c83e5245a0f44d291f9
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-08-10 10:39:35 +0530

    25173

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import RestaurantInfo
+def homepage(request):
+    restaurant=RestaurantInfo.objects.first()
+    return render(request,'home.html',{'restaurant_name':restaurant_name if restaurant else 'Our restaurant'})
