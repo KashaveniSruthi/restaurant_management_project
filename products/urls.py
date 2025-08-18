3063a1924b2f9b31173826c509549aabed12a94c
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-08-18 15:01:51 +0530

    33484

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,33 @@
-from django.urls import path
-from .views import *
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
-]
+from .models import Item
+from .serializers import ItemSerializer
+
+'''
+NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
+'''
+
+# Create your views here.
+class ItemView(APIView):
+
+    def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class MenuView(APIView):
+    def get(self,request):
+        menu_items=[
+            {"name":"Veg biryabi","price":200},
+            {"name":"Chicken Biryani","price":300}
+        ]
+        return Response(menu_items,status=status.HTTP_200_OK)
