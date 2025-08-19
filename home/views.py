7e0483214bfdae1e837f1e94417aeba07bf6c11a
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2025-08-19 10:57:48 +0530

    33487

--- home/views.py
+++ home/views.py
@@ -8,8 +8,13 @@ def homepage(request):
     return render(request,'home.html',{'restaurant_name':restaurant.name if restaurant else 'Our restaurant'})
 def about(request):
     restaurant=RestaurantInfo.objects.first()
-    return  render(requesr,'about.html',{
+    return  render(request,'about.html',{
         'restaurant_name':restaurant.name if restaurant else 'Our restaurant',
         'description':restaurant.description if restaurant else '',
         'image': restaurant.image.url if restaurant and restaurant.image else ''
     })
+def index(request):
+    context={
+        "phone"=getattar(settings,"RESTAURANT_PHONE","N/A")
+    }
+    return render(request,'home/index.html',context)
